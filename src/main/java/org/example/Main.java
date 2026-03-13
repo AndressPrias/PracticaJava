9b151ff3c2703f15aec653ee731b9dace33a0eb4
Andrest <anpriasca@gmail.com>
2026-03-13 06:05:08 -0500

    cambio de programa a programa de ventas de una tienda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,24 +10,7 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        int opc = -1;
-        int num = 0;
-        while (opc != 2){
-
-            System.out.println("ingresa un numero? ");
-            num = input.nextInt();
-
-            List<String> list = new ArrayList<String>();
-            for (int i = 1; i <= num; i++){
-                list.add(input.next());
-            }
-
-            }
-
-
-            System.out.println("Desea continuar? (1=Si/2=No) ");
-            opc = input.nextInt();
+        
 
         }
     }
-}
